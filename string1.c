d17b6a6e90e3d9806ccf1a3bd7f990c4b19b593a
kevDevops <kelvinnyagaka7@gmail.com>
2023-11-30 20:48:50 -0800

    The _string.repeat method returns a string which contains the specified number of copies of the string on which it was called, concatenated together.

---
+++ string1.c
@@ -0,0 +1,46 @@
+#include "shell.h"
+
+/**
+ * _strcpy -it copies a string
+ * @dest: the only destination
+ * @src: its source
+ *
+ * Return: pointers to destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	if (dest == src || src == 0)
+		return (dest);
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = 0;
+	return (dest);
+}
+
+/**
+ * _strdup -to duplicate a string
+ * @str: the string to be duplicated
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
